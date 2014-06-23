299b67af84c51d490ec3edbc820a449084ac6cf6
Yury Rodrigues <yuryrodrigues28@gmail.com>
2014-06-22 23:19:40 -0300

    01

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -5,13 +5,10 @@ define([ ], function () {
     var constants = {};
     constants.VERSION = '0.5.0';
 
-<<<<<<< HEAD
     constants.URL = 'https://laverna.cc/';
     constants.DROPBOX_KEY = 'pzscbhwtiq2mflf';
-=======
     constants.URL = location.origin + location.pathname.replace('index.html', '');
-    constants.DROPBOX_KEY = '10iirspliqts95d';
->>>>>>> upstream/master
+    //constants.DROPBOX_KEY = '10iirspliqts95d';
     // constants.DROPBOX_SECRET = null;
 
     return constants;
